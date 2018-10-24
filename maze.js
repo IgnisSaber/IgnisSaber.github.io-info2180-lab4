9a99c39a1d1e8442afd7058d399d447027711378
Candice de Lisser <candice.t.delisser@gmail.com>
2018-10-23 21:47:18 -0500

    Alerts on successful completion of maze
    Exercise 3

--- maze.js
+++ maze.js
@@ -1,27 +1,69 @@
-//Exercise 2
-//this function is fine
+
+//Exercise 3
+
+var loss = false;
+
 function lossAlert(){
+loss = true;
+//alert(loss);
 var x = document.querySelectorAll(".boundary");
 var i;
 for(i=0; i < x.length; i++){
-	x[i].style.backgroundColor="#ff8888";
-	
+	x[i].classList.add("youlose");
 	}
+//alert("You lost bro!");
 };
 
+//come baCK to this, Candice
 function mouseOver() {
+	document.getElementById("end").onmouseover = function(){touchEnd()};
 	var y = document.querySelectorAll(".boundary");
 	var j;
 	for(j=0; j < y.length; j++){
 		y[j].onmouseover = function(){lossAlert()};
 		
-		}
+	}
 };
+
+
+
+//new function
+
+
+
+function touchEnd(){
+	if (loss===true){
+		alert("You lost... Nice try!");
+	}
+	else{
+		alert("You win, mate!");
+	}
+};
+
+
 
 
 window.onload = mouseOver;
 
+//Exercise 2
+//this function is fine
+//function lossAlert(){
+//var x = document.querySelectorAll(".boundary");
+//var i;
+//for(i=0; i < x.length; i++){
+//	x[i].style.backgroundColor="#ff8888";
+	
+//	}
+//};
 
+//function mouseOver() {
+//	var y = document.querySelectorAll(".boundary");
+//	var j;
+//	for(j=0; j < y.length; j++){
+//		y[j].onmouseover = function(){lossAlert()};
+//		
+//		}
+//};
 
 
 //Exercise 1
